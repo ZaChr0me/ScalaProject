2d293a36a241fd7766a54c61ebc12244822785aa
Dayonos <lavillelucas@gmail.com>
2022-01-24 14:45:32 +0100

    Added a few parameters

--- src/main/scala/airportProject/model/Runways.scala
+++ src/main/scala/airportProject/model/Runways.scala
@@ -7,7 +7,23 @@ class Runway(
     id: Long,
     airportRef: Long,
     airportIdent: String,
-    surface: String
+    surface: String,
+    length: Int = 0,
+    width: Int = 0,
+    lighted: Int = 0,
+    closed: Int = 0,
+    leIdent: String = "",
+    leLatitude: Float = 0,
+    leLongitude: Float = 0,
+    leElevation: Int = 0,
+    leHeadingDegT: Float = 0,
+    leDisplacedThreshold: Int = 0,
+    heIdent: String = "",
+    heLatitude: Float = 0,
+    heLongitude: Float = 0,
+    heElevation: Int = 0,
+    heHeadingDegT: Float = 0,
+    heDisplacedThreshold: Int = 0
 )
 
 object Runway:
