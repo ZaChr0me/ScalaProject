f2820af184a583d084877be1383ab6c7e436aa05
Dayonos <lavillelucas@gmail.com>
2022-02-07 15:19:39 +0100

    Added a few parsers for user requests

--- src/main/scala/airportProject/Main.scala
+++ src/main/scala/airportProject/Main.scala
@@ -107,18 +107,25 @@ def main(args: Array[String]): Unit = {
   printf("project console entry point. TEMPORARY.")
 
   //input user choice between query and reports
-  
+  val reportOrQuery = false
+
+  val reportType = ""
   //for now, and to go faster, main is used as manual testing, while the core app
   //isn't done, and the work on gui hasn't started
 
-  val query_code: String = ""
-  val query_name: String = ""
+  val queryCodeOrName: String = ""
   //in consideration with ui, boolean indicating if code or name
-  val code_or_name: Boolean = false
+  val codeOrName: Boolean = false
 
   val query_name_fuzzy: String = ""
 
+  reportOrQuery.match {
+    case true => reportWork(database, reportType)
+    case false => queryWork(queryCodeOrName, codeOrName, database)
+  }
+
 }
+
 
 def queryWork(country_code_or_name: String, code_or_name: Boolean, database: Database) = {
   code_or_name.match {
@@ -128,10 +135,18 @@ def queryWork(country_code_or_name: String, code_or_name: Boolean, database: Dat
 
 }
 
-def queryCode(countryCode: String, database: Database) = {
+def reportWork(database: Database, reportType: String) = {
+  reportType.match {
+    case "countries" => database.parseReportCountries()
+    case "surface" => database.parseReportSurface()
+    case "latitude" => database.parseReportLatitude()
+  }
+}
 
+def queryCode(countryCode: String, database: Database) = {
+  val result: List[(Airport, List[Runway])] = database.parseCountryCode(countryCode)
 }
 
 def queryName(countryName: String, database: Database) = {
-
+  val result: List[(Airport, List[Runway])] = database.parseCountryName(countryName)
 }
--- src/main/scala/airportProject/memoryDatabase/Database.scala
+++ src/main/scala/airportProject/memoryDatabase/Database.scala
@@ -5,9 +5,9 @@ import airportProject.service._
 import scala.io.Source
 
 class Database (
-    airports: Array[Airport],
-    countries: Array[Country],
-    runways: Array[Runway]
+    airports: List[Airport],
+    countries: List[Country],
+    runways: List[Runway]
 )
 
 object Database:
@@ -19,6 +19,25 @@ object Database:
         )
     }
 
-    def parseCountryCodeAirports(countryCode: String): Array[Airport] = airports.dropWhile(airport => {airport.iso_country != countryCode})
+    def parseCountryCode(countryCode: String): List[(Airport, List[Runway])] = airports
+    airports.dropWhile(airport=>{airport.isoCountry!=countryCode})
+    .map(airport=>(airport,runways.filter{x => (x.airportIdent == airport.ident && x.airportRef==airport.id)}))
+
+    def parseCountryName(countryName: String): List[(Airport, List[Runway])] = airports
+    .dropWhile(airport => {airport.name != countryName})
+    .map(airport => (airport, runways.filter{runway => (runway.airportRef == airport.id && runway.airportIdent == airport.ident)}))
     
-    def parseCountryCodeRunways(countryCode: String): Array[Runway] = parseCountryCodeAirports(countryCode)
+    def parseReportCountries(): Option[List[(Country, Int)]] = countries.match {
+        case (countries.count >= 20) => countries
+        .map(country => (country, airports.filter(airport => (airport.isoCountry == country.code)).count))
+        .sortWith((_, _ > _))
+        .filter(_._2 < 10 && _._2 > (countries.count - 10)).map(_._1)
+        case _ => None
+    }
+    
+    def parseReportSurface(): List[(Country, List[String])] = countries
+    .map(country => (country, runways.filter(runway => airports.filter(airport => (airport.isoCountry == country.code)).Ident.contains(runway.airportIdent)).surface.map(_.toUpperCase).distinct))
+
+    def parseReportLatitude(): List[(String, Int)] = runways
+    .map(runway => (String(runway.leIdent)), runway.filter(x => x.leIdent == runway.leIdent).count)
+    .distinct
