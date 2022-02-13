063bac75d23d9679ac86de194122a0cb64451247
Name <non@non.non>
2022-02-13 18:23:52 +0100

    Added tests for database

--- src/test/scala/airportProject/memoryDatabase/DatabaseSpec.scala
+++ src/test/scala/airportProject/memoryDatabase/DatabaseSpec.scala
@@ -1,5 +1,7 @@
 package airportProject.memoryDatabase
 
+import airportProject.model._
+import airportProject.service._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
@@ -9,6 +11,69 @@ class DatabaseSpec extends AnyFlatSpec with Matchers {
     Database
       .initializeFromCsv(100, "airports.csv", "countries.csv", "runways.csv")
       .isLeft must be(true)
+  }
+
+  "Database" should "Initialize successfully at least 60 percent of the 3 given csv" in {
+    Database
+      .initializeFromCsv(60, "airports.csv", "countries.csv", "runways.csv")
+      .isLeft must be(false)
+  }
+
+  "Database" should "Initialize successfully 100 percent of the 3 test csv" in {
+    Database
+      .initializeFromCsv(
+        100,
+        "tests/airportsTest.csv",
+        "tests/countriesTest.csv",
+        "tests/runwaysTest.csv"
+      )
+      .isLeft must be(false)
+  }
+
+  "Database" should "Send every airport and runways of a country using his country code" in {
+    val database = Database
+      .initializeFromCsv(
+        100,
+        "tests/airportsTest.csv",
+        "tests/countriesTest.csv",
+        "tests/runwaysTest.csv"
+      )
+      .right
+      .get
+      
+
+    database.parseCountryCode(IsoCountryType.orNone("US").get).size must be(3)
+    database.parseCountryCode(IsoCountryType.orNone("US").get).foreach((a, r) => r.size must be(1))
+  }
+
+  "Database" should "Send every airport and runways of a country using his country name" in {
+    val database = Database
+      .initializeFromCsv(
+        100,
+        "tests/airportsTest.csv",
+        "tests/countriesTest.csv",
+        "tests/runwaysTest.csv"
+      )
+      .right
+      .get
+      
+
+    database.parseCountryName(NonEmptyString.orNone("United States").get).size must be(3)
+    database.parseCountryName(NonEmptyString.orNone("United States").get).foreach((a, r) => r.size must be(1))
+  }
+
+  "Database" should "Give the first 10 and last 10 countries" in {
+      val database = Database
+      .initializeFromCsv(
+        60,
+        "airports.csv",
+        "countries.csv",
+        "runways.csv"
+      )
+      .right
+      .get
+
+      database.parseReportCountries().size must be(20)
   }
 
 }
